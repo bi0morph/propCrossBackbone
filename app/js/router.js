d544745e1f8da10091f8c7c4de4c7e2f2be20208
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-03-16 13:41:13 +0200

    Add berore and after render func

--- app/js/router.js
+++ app/js/router.js
@@ -4,8 +4,10 @@ app.Router = Backbone.Router.extend({
     },
     defaultRoute: function defaultRoute(actions){
         app.preloader.show();
+        var mainModel = new app.models.MainModel();
         app.views.mainView = new app.views.MainView({
         	el: $('#some-app-container'),
+            model: mainModel
         });
         app.preloader.hide();
     }
--- app/js/views/defaultView.js
+++ app/js/views/defaultView.js
@@ -1,15 +1,28 @@
 app.views.DefaultView = Backbone.View.extend({
     initialize: function initialize() {
-        this.beforeRender();
-        this.render();
-        this.afterRender();
+    	var _this = this; 
+    	
+    	_.bindAll(this, 'beforeRender', 'render', 'afterRender');
+        this.render = _.wrap(this.render, function(render) { 
+            _this.beforeRender(); 
+            render(); 
+            _this.afterRender(); 
+            return _this; 
+        }); 
+
+        this.listenTo( this.model, 'change', this.render);
+        this.model.fetch();
     },
-    beforeRender: function beforeRender() {},
-    afterRender: function afterRender() {},
-    variables: {},
-    template: null,
     render: function render(){
-        // what if template is empty?
-        this.$el.html( _.template( this.template)(this.variables) );
+        var variables = this.model.toJSON();
+
+        this.$el.html( _.template( this.template)(variables) );
+        return this;
+    },
+    beforeRender: function beforeRender(){
+    	return this;
+    },
+    afterRender: function afterRender(){
+    	return this;
     },
 });
