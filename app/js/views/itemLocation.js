4708391857e27454c02f282e589b4168303abba1
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-04-02 18:41:49 +0300

    Add links to location item

--- app/js/views/itemLocation.js
+++ app/js/views/itemLocation.js
@@ -1,6 +1,6 @@
 app.views.ItemLocation = Backbone.View.extend({
 	tagName: 'li',
-	template: 'Location: <%= place_name %> "<%= long_title %>"',
+	template: '<a href="#search_query/<%= encodeURI(place_name) %>">Location: <%= place_name %> "<%= long_title %>"</a>',
 	render: function render() {
         var variables = this.model.toJSON();
         this.$el.html( _.template( this.template)(variables) );
